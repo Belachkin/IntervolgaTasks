ccfdb9ff9bc6c8093ea0a8137d67f0c3fd395135
Danila <belkin.job123@gmail.com>
2021-10-24 13:56:10 +0300

    add main page

--- index.php
+++ index.php
@@ -2,7 +2,41 @@
 require_once 'app/header.php';
 ?>
 
-Hi
+<div class="container px-4 py-5" id="featured-3">
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="feature col">
+        <h2>Найти возрастающую последовательность</h2>
+        <p>Напишите код на php, который найдет самую длинную возрастающую последовательность в массиве целых чисел из 100 элементов.</p>
+        <a href="/task1/index.php" class="icon-link">
+          Перейти <
+        </a>
+      </div>
+      <div class="feature col">  
+        <h2>PHP и HTML. Напишите код на PHP</h2>
+        <p>В переменной $a лежит текст новости. В переменной $link лежит ссылка на страницу с полным текстом этой новости.
+            Нужно в переменную $b записать сокращенный текст новости по правилам...</p>
+        <a href="/task2/index.php" class="icon-link">
+            Перейти <        
+        </a>
+      </div>
+      <div class="feature col">
+        <h2>Веб-разработка</h2>
+        <p>На диске лежит файл image.png, размер 20000 на 20000. Вывести картинку как баннер размером 200 на 100 пикселей.  
+        Обратите внимание на размер и пропорции.</p>
+        <a href="/task3/index.php" class="icon-link">
+            Перейти <        
+        </a>
+      </div>
+      <div class="feature col">
+        <h2>Массивы</h2>
+        <p>Дан массив из 100 элементов. Требуется вывести количество пар одинаковых элементов.
+        Например: 1, 1, 2, 3, 4, -51, 12, 12, 12, -51</p>
+        <a href="/task4/index.php" class="icon-link">
+            Перейти <        
+        </a>
+      </div>
+    </div>
+  </div>
 
 <?php 
 require_once 'app/footer.php';
